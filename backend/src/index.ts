6dc855df962b574e315d18bddeb9e86630716fd2
CodeAritraDhank <aridhank21205@gmail.com>
2025-10-01 12:03:17 +0530

    app initialised

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-import './app';
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.use("/", (req: express.Request, res: express.Response) => {
+  res.send("hii");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App is running at http://localhost:${PORT}`);
+});
