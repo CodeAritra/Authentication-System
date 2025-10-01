5ba2c9272248d020b1a9d324dbeb124b8f3615f1
CodeAritraDhank <aridhank21205@gmail.com>
2025-10-01 09:17:18 +0530

    Initialize

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+const PORT = 5000
+
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(()=>app.listen(PORT,()=>console.log(`Server running in ${PORT}`)))
+  .catch((err) => console.error("Error in connecting db", err));
