ac607d8524b9bb0c3b76bacb45846e82ed1f7810
CodeAritraDhank <aridhank21205@gmail.com>
2025-10-02 09:29:58 +0530

    auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,30 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRouter from "./routes/user.route"
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use("/", (req: express.Request, res: express.Response) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send("hii");
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`App is running at http://localhost:${PORT}`);
-});
+app.use("/api/auth",authRouter)
+
+const PORT = process.env.PORT || 2000;
+// app.listen(PORT, () => {
+//   console.log(`App is running at http://localhost:${PORT}`);
+// });
+
+mongoose
+  .connect(process.env.MONGO_URI || "")
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is running at http://localhost:${PORT}`);
+    });
+    console.log("Db connected")
+  })
+  .catch((err) => console.log("Mongodb connection error"));
