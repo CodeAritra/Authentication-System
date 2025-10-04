97ba800251556088797faa4d018d69ddcfb72cb2
CodeAritraDhank <aridhank21205@gmail.com>
2025-10-04 09:28:03 +0530

    Revert "Merge pull request #1 from CodeAritra/main"
    This reverts commit 3be68d5b7a37bd1a164d9ab12b26abfd6b5de291, reversing
    changes made to da80b9a0e0f0c3b87cd3cf1367398eae558f967a.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <form className="card p-6 w-full max-w-md" >
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <input
+          className="input input-bordered w-full mb-2"
+          placeholder="Email"
+        />
+        <input
+          className="input input-bordered w-full mb-4"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="btn btn-primary w-full">Login</button>
+      </form>
+    </div>
+  );
+}
